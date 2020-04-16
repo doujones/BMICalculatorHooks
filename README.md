e757764609f3b0b413cd8dbf66cc1e895ead8f29
Dou Jones <MISTERCHIEFEXECUTIVEOFFICER@YAHOO.COM>
2020-04-16 07:06:21 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# BMICalculatorHooks
-Created with CodeSandbox
---
+++ src/components/BmiCalculator.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BmiCalculator = () => {
+  return <div>This is the BMI calculator component</div>;
+};
+
+export default BmiCalculator;
